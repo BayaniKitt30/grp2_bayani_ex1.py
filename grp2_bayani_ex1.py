8ea6f07e448f6e1f08ead04a57bf0e0762f45ccd
BayaniKitt30 <bayanikitt_bscs@plmun.edu.ph>
2024-09-21 19:24:09 +0800

    Add files via upload

---
+++ grp2_bayani_ex1.py
@@ -0,0 +1,35 @@
+#Python Comments
+# a. Create a simple print script
+# b. Add comment "My first simple script"
+print("Hello, I'm Kitt Bayani! from BSCS 3G") #My first simple script
+
+#Python Variables
+# a. Create two variables and assign a number and a name to them
+number = 22
+name = "Kitt"
+
+# b. Then print both values
+print("Name:",  name)
+print("Number:", number)
+
+#Python - Variable Names
+# a. Create 4 variables names using the first names of your group members. Your name should not be included.
+Groupmates = "Groupmates"
+Juliana = "Juliana"
+Cyril Anne = "Cyril Anne"
+Neo = "Neo"
+
+print("Groupmates:")
+print("Juliana")
+print("Cyril Anne")
+print("Neo")
+
+# Many values to multiple variables
+# a. Assign values to multiple variables in one line
+x, y, z = 100, 200, 300
+print("x:",x, "y:",y ,"z:",z)
+
+# One value tp multiple Variables 
+# a. Assign the same value to multiple variables in one line
+a = b = c = "100"
+print("a:", a, "b:", b, "c:", c)
